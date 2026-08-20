9c056346fa7a76ac764690cccfb1453004873242
Edyeex <casagrandader461@gmail.com>
2026-08-20 16:53:37 -0300

    Obtendo dados de formuário HTML

---
+++ ObtendoDadosFormulario/cad.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP</title>
+</head>
+<body>
+
+    <header>
+        <h1>Resultado</h1>
+    </header>
+    <main>
+        <?php 
+            $nome = $_REQUEST["nome"] ?? "Nome não informado"; // Passando o valor de nome do HTML para $nome do PHP
+            $sobrenome = $_REQUEST["sobrenome"] ?? "Sobrenome não informado"; // Passando o valor de sobrenome do HTML para $sobrenome do PHP
+            
+            echo "Seja bem vindo $nome $sobrenome! Esse é um teste de PHP"
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar</a></p> <!-- "Botão" para voltar para a pagina anterior -->
+    </main>
+    
+</body>
+</html>
